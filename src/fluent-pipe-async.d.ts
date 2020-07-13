75c21acbfe2daad1c8b8221af03b1559cf93f1dc
David Walker <dwalker@vorteon.com>
2020-07-12 20:52:15 -0400

    wip

--- src/fluent-pipe-async.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class FluentPipeAsync<T, R> {
-    result: Promise<R>;
-    constructor(initialState: Promise<T>, func: (value: T) => Promise<R>);
-    private exec;
-    pipe<R2>(func: (value: R) => R2): FluentPipeAsync<R, R2>;
-    pipeAsync<R2>(func: (value: R) => Promise<R2>): FluentPipeAsync<R, R2>;
-    static from<T>(initialState: T): FluentPipeAsync<T, T>;
-}
-//# sourceMappingURL=fluent-pipe-async.d.ts.map
--- src/fluent-pipe.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class FluentPipe<T, R> {
-    result: R;
-    constructor(initialState: T, func: (value: T) => R);
-    pipe<R2>(func: (value: R) => R2): FluentPipe<R, R2>;
-    static from<T>(initialState: T): FluentPipe<T, T>;
-}
-//# sourceMappingURL=fluent-pipe.d.ts.map
